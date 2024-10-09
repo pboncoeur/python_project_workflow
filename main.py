6274431963e979b562c28cabd65969126df56821
pboncoeur <pboncoeur@udm.ac.mu>
2024-10-09 11:06:14 +0400

    Commit changes to main and test_main, add functionality

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def greet(name):
     return f"Hello, {name}!"
 
+def add(x, y):
+    return x + y
+
 if __name__ == "__main__":
-    print(greet("World"))
+    print(greet("Test"))
+    print(add(2, 3))
--- test_main.py
+++ test_main.py
@@ -1,10 +1,15 @@
 import unittest
-from main import greet
+from main import greet, add
 
 class TestGreetFunction(unittest.TestCase):
     def test_greet(self):
         self.assertEqual(greet("World"), "Hello, World!")
-        self.assertEqual(greet("Alice"), "Hello, Alice!")
+        self.assertEqual(greet("Test"), "Hello, Test!")
+
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+    
 
 if __name__ == "__main__":
     unittest.main()
